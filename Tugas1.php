4073525f2dfc115d134484d4ad9adc435d4424b6
sifakhoirulmuna29 <109189084+sifakhoirulmuna29@users.noreply.github.com>
2022-08-10 15:32:22 +0700

    Add files via upload

---
+++ Tugas1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=i, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+$i =99;
+while($i >= 1){
+    echo "$i";
+    echo "<br/>";
+    $i-=2;
+}
+?>
+</body>
+</html>
---
+++ Tugas2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+     $a = 3;
+     $i-= 1 ;
+
+     for ($i , $a; $i <= 10; $i++){
+        if ($i){
+            echo pow ($a,$i)."<br>";
+        }
+     }
+?>
+</body>
+</html>
